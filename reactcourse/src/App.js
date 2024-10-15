97dee960cd4dda356dfab6b6a5f75864b396e942
sneha kishore <snehakishore31@gmail.com>
2024-10-16 02:03:34 +0530

    third commit

--- reactcourse/src/App.js
+++ reactcourse/src/App.js
@@ -1,13 +1,23 @@
+import { useEffect, useState } from "react";
 
+export default function App(){
+  const [advice, setAdvice] = useState("");
+  const [count, setCount] = useState(0);
 
-export default function App() {
-  return (
-    <div className="App">
-      <h1>Hello im sneu</h1>
-      <h2>i am good!</h2>
-    </div>
-  );
+  async function getAdvice(){
+    const res = await fetch('https://api.adviceslip.com/advice')
+    const data = await res.json();
+    setAdvice(data.slip.advice);
+    setCount((c)=>c+1);
+  }
+
+ return( <div>
+    <h1>{advice}</h1>
+    <h3>by codersneu</h3>
+    <button onClick={getAdvice}>get my two sents</button>
+    <p>you've read <strong>{count}</strong> cents</p>
+  </div>
+ )
 }
-
 
 
