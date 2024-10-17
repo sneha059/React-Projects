ec1c661e384c745ecd62907a21cfe3066f89bff6
sneha kishore <snehakishore31@gmail.com>
2024-10-17 16:00:17 +0530

    commits again

--- reactcourse/src/App.js
+++ reactcourse/src/App.js
@@ -1,23 +1,31 @@
-import { useEffect, useState } from "react";
+import { startTransition, useEffect, useState } from "react";
 
-export default function App(){
+export default function App() {
   const [advice, setAdvice] = useState("");
   const [count, setCount] = useState(0);
 
-  async function getAdvice(){
+  async function getAdvice() {
     const res = await fetch('https://api.adviceslip.com/advice')
     const data = await res.json();
     setAdvice(data.slip.advice);
-    setCount((c)=>c+1);
+    setCount((c) => c = 1)
   }
 
- return( <div>
+useEffect(function () {
+  getAdvice();
+}, []);
+
+  return (<div>
     <h1>{advice}</h1>
     <h3>by codersneu</h3>
     <button onClick={getAdvice}>get my two sents</button>
-    <p>you've read <strong>{count}</strong> cents</p>
+    <Message count={count} />
   </div>
- )
+  );
 }
-
+function Message(props) {
+  return (
+    <p> you've read <strong>{props.count}</strong> cents </p>
+  );
+}
 
